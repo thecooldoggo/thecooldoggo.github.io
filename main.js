925cbc123e180942b3a532ce846ac8c6a31daf36
TheCoolDoggo <130795558+thecooldoggo@users.noreply.github.com>
2024-05-29 23:09:22 -0400

    teeny oopsie

--- main.js
+++ main.js
@@ -3,10 +3,19 @@ const delay = 600000;
 
 let getSubscribers = () => {
     fetch(`https://thecooldoggo.hackclub.app/yt-stats.json`)
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
     .then(data => {
-        console.log(data);
-        subCount.innerHTML = data["items"][0].subscriber_count;
+        if (data && data.subscriber_count && data.view_count) {
+            subCount.innerHTML = data.subscriber_count;
+            // viewCount.innerHTML = data.view_count;
+        } else {
+            throw new Error('JSON response does not have expected structure');
+        }
     })
     .catch(error => console.error('Error while fetching:', error));
 }
